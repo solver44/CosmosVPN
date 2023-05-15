5c268ece846ad0b5651d07fababf015498f3b13d
Sunnat <74422066+solver44@users.noreply.github.com>
2023-05-15 12:32:54 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudoers_content=$(cat << EOL
+sudo bash -c "cat > /etc/sudoers.d/cosmosvpn" << EOL
 www-data ALL=(root) NOPASSWD: /var/www/returnConnectedClients.sh
 www-data ALL=(root) NOPASSWD: /var/www/createClient.sh
 www-data ALL=(root) NOPASSWD: /var/www/returnClientTemplate.sh
@@ -10,5 +10,16 @@ www-data ALL=(root) NOPASSWD: /var/www/setVPNProtocol.sh
 www-data ALL=(root) NOPASSWD: /var/www/setVPNDNS.sh
 www-data ALL=(root) NOPASSWD: /var/www/toggleVPNStatus.sh
 EOL
-)
-echo "${sudoers_content}" "sudo tee /etc/sudoers.d/cosmosvpn && sudo chmod 0440 /etc/sudoers.d/cosmosvpn"
+
+# sudoers_content=$(cat << EOL
+# www-data ALL=(root) NOPASSWD: /var/www/returnConnectedClients.sh
+# www-data ALL=(root) NOPASSWD: /var/www/createClient.sh
+# www-data ALL=(root) NOPASSWD: /var/www/returnClientTemplate.sh
+# www-data ALL=(root) NOPASSWD: /var/www/deleteClient.sh
+# www-data ALL=(root) NOPASSWD: /var/www/setVPNPort.sh
+# www-data ALL=(root) NOPASSWD: /var/www/setVPNProtocol.sh
+# www-data ALL=(root) NOPASSWD: /var/www/setVPNDNS.sh
+# www-data ALL=(root) NOPASSWD: /var/www/toggleVPNStatus.sh
+# EOL
+# )
+# echo "${sudoers_content}" "sudo tee /etc/sudoers.d/cosmosvpn && sudo chmod 0440 /etc/sudoers.d/cosmosvpn"
