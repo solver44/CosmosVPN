8a17c975ca3f4684328fce73b3d52bdbbe3fb271
solver44 <s.erkinov.77@gmail.com>
2023-05-25 08:12:06 +0500

    new debug

--- install_all.sh
+++ install_all.sh
@@ -78,8 +78,8 @@ curl -O https://raw.githubusercontent.com/solver44/CosmosVPN/main/install_dir.sh
 chmod +x install_dir.sh
 ./install_dir.sh
 
-# sudo touch /etc/openvpn/credentials.txt
-# sudo chmod 600 /etc/openvpn/credentials.txt
+sudo touch /etc/openvpn/credentials.txt
+sudo chmod 600 /etc/openvpn/credentials.txt
 
 # sudo apt-get install expect -y
 
--- openvpn_scripts/createClient.sh
+++ openvpn_scripts/createClient.sh
@@ -1,43 +1,16 @@
 #!/bin/bash
+if [ -z "$1" ]; then
+  echo "Usage: <script> username"
+  exit 1
+fi
 
-_ask_user() {
-    printf "\nCREATE USER\n\n"
-    read -p "Username : " User
-}
-_ask_pass() {
-    read -p "Password (leave blank for random): " Pass
-    [[ -z $Pass ]] && Pass=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8)
-}
-_ask_days() {
-    read -p "How many days : " Days
-}
 
-clear
-_ask_user
-if [[ $(grep -c "^$User" /etc/passwd) -ne 0 ]]; then
-    clear
-    printf "\nUsername already taken" && sleep 2 && printf "\nUsername already taken\r\e[K"
-    user_create
-else
-    _ask_pass
-    _ask_days
+function outputConfig() {
+    CLIENT=$1
+    PASSWORD=$2
 
-    IP=$(wget -4qO- http://ipinfo.io/ip)
-    Exp=$(date +%Y-%m-%d -d "$Days days")
-    useradd -m -s /bin/false -e "$Exp" "$User" -p $(openssl passwd -1 "${Pass}") &>/dev/null
-    Date=$(chage -l "$User" | grep -i "account expires" | awk -F": " '{print $2}')
-
-    printf "\n"
-    printf " Account Created!\n\n"
-    printf "Host/IP   : %s \n" "$IP"
-    printf "Username  : %s \n" "$User"
-    printf "Password  : %s \n" "$Pass"
-    printf "Expires on: %s \n" "$Date"
-    printf "Ovpn config: \n"
-
-    cd /etc/openvpn/easy-rsa/ || return
-    echo yes | ./easyrsa build-client-full "$User" nopass >/dev/null
-
+    echo "$PASSWORD"
+    cat /etc/openvpn/client-template.txt
     echo "<ca>"
     cat "/etc/openvpn/easy-rsa/pki/ca.crt"
     echo "</ca>"
@@ -50,6 +23,37 @@ else
     echo "<tls-crypt>"
     cat /etc/openvpn/tls-crypt.key
     echo "</tls-crypt>"
+}
+
+if [[ $(grep -c "^$User" /etc/passwd) -ne 0 ]]; then
+    clear
+    echo "User already exists"
+    PASSWORD=$(grep -E "^${User}:" /etc/openvpn/credentials.txt | cut -d: -f2)
+    outputConfig "$User" "$PASSWORD"
+    exit
+else
+    User=$1
+    Pass=$(openssl rand -base64 16)
+    Days=365
+
+    # IP=$(wget -4qO- http://ipinfo.io/ip)
+    Exp=$(date +%Y-%m-%d -d "$Days days")
+    useradd -m -s /bin/false -e "$Exp" "$User" -p $(openssl passwd -1 "${Pass}") &>/dev/null
+    # Date=$(chage -l "$User" | grep -i "account expires" | awk -F": " '{print $2}')
+
+    # printf "\n"
+    # printf " Account Created!\n\n"
+    # printf "Host/IP   : %s \n" "$IP"
+    # printf "Username  : %s \n" "$User"
+    # printf "Password  : %s \n" "$Pass"
+    # printf "Expires on: %s \n" "$Date"
+    # printf "Ovpn config: \n"
+
+    cd /etc/openvpn/easy-rsa/ || return
+    echo yes | ./easyrsa build-client-full "$User" nopass >/dev/null
+
+    echo "${User}:${Pass}" >>/etc/openvpn/credentials.txt
+
+    outputConfig "$User" "$Pass"
     exit 0
-    return
 fi
